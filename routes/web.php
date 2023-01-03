902391ba4aea45347ac3e74a0d51e153d5cfbdc5
Gerry Bong <ggbong734@gmail.com>
2023-01-02 17:50:17 -0800

    added test data in router web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () { // second argument below is the data returned
+    return view('listings',[
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Hello how are you healllo elllo hlllo hllloe',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Two Hello how are you healllo elllo hlllo hllloe',
+            ]
+        ]
+    ]);
+});
+
+Route::get('/hello', function () {
+    return response('<h1>Hello World</h1>', 200)
+    ->header('Content-Type', 'text/plain')
+    ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function($id){
+    // die and dump method for debugging
+    // dd($id);
+    // ddd($id) // die dump and debug;
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+
+Route::get('/search', function(Request $request) {
+    dd($request->name . ' ' . $request->city);
 });
